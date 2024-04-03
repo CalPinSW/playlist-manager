d435d31e7e11240cac9abb06f24fa719ffa85d62
Calum Pinder <calum.pinder@softwire.com>
2024-04-03 09:12:30 +0100

    Make image height and width optional

--- backend/src/dataclasses/image.py
+++ backend/src/dataclasses/image.py
@@ -1,7 +1,8 @@
+from typing import Optional
 from pydantic import BaseModel
 
 
 class Image(BaseModel):
     url: str
-    height: int
-    width: int
+    height: Optional[int] = None
+    width: Optional[int] = None
