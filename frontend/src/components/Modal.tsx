2063785b36f30b07e433ea3fce8a934472e9f751
Calum Pinder <calum.pinder@softwire.com>
2024-05-01 17:16:58 +0100

    Return ansible playlist manager version to main:

--- frontend/src/components/Modal.tsx
+++ frontend/src/components/Modal.tsx
@@ -12,11 +12,11 @@ const Modal: FC<ModalProps> = ({ children, isModalOpen, closeModal }) => {
     <>
       <div
         onClick={closeModal}
-        className={`fixed top-0 left-0 w-[100vw] h-[100vh] z-40 bg-black opacity-20`}
+        className={`fixed top-0 left-0 w-screen h-screen z-40 bg-black opacity-20`}
       ></div>
       <div
         onClick={(e) => e.stopPropagation()}
-        className={`bg-background-popover w-fit h-fit fixed opacity-100 left-[40vw] top-[40vh] rounded-xl shadow p-6 z-50`}
+        className={`bg-background-popover w-fit h-fit fixed opacity-100 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-xl shadow p-6 z-50`}
       >
         {children}
       </div>
