6428b6d970279e4bba5d9e6aa5bba5f7b9587837
Calum Pinder <calum.pinder@softwire.com>
2025-04-11 13:35:39 +0100

    move auth0 creds to env

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -5,14 +5,16 @@ import AppRoutes from "./AppRoutes";
 import { Auth0Provider } from "@auth0/auth0-react";
 import { SpotifyAuthContextProvider } from "./context/SpotifyAuthcontext";
 
+const auth0Domain = process.env.AUTH0_DOMAIN ?? "";
+const auth0ClientId = process.env.AUTH0_CLIENT_ID ?? "";
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <React.StrictMode>
       <Auth0Provider
-        domain="dev-3tozp8qy1u0rfxfm.us.auth0.com"
-        clientId="vlvk6JVXllIpfJEElGtEZnjmfG5NvVo3"
+        domain={auth0Domain}
+        clientId={auth0ClientId}
         authorizationParams={{
           redirect_uri: "http://localhost:8080/login",
           audience: "https://playmanbackend.com"
