d24ef0a2ad1a3f6a324e7522dde5f37c3deb52b8
Calum Pinder <calum.pinder@softwire.com>
2025-04-16 21:46:17 +0100

    Tidy up filenames

--- frontend/src/hooks/useSpotifyAuthContext.ts
+++ frontend/src/hooks/useSpotifyAuthContext.ts
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { SpotifyAuthContext } from "../context/SpotifyAuthcontext";
+import { SpotifyAuthContext } from "../context/SpotifyAuthContext";
 
 export const useSpotifyAuthContext = () => {
     const spotifyAuthContext = useContext(SpotifyAuthContext) 
