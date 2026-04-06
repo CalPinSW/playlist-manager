465d7317c68e1e820ac912bdedda7ad4be7e5c2a
CalPinSW <calpinsw@gmail.com>
2026-04-06 16:11:06 +0100

    Add reauthorize spotify button

--- playlist-manager-next/app/(pages)/spotify-settings/_components/SpotifyConnectedSettings.tsx
+++ playlist-manager-next/app/(pages)/spotify-settings/_components/SpotifyConnectedSettings.tsx
@@ -3,6 +3,7 @@ import { UserProfile } from '@spotify/web-api-ts-sdk';
 import React, { FC } from 'react';
 import AsyncButton from '../../components/AsyncButton';
 import Image from 'next/image';
+import { spotifyScopes } from '../../../../lib/spotify';
 
 interface SpotifyConnectedSettingsProps {
   user: UserProfile;
@@ -16,6 +17,20 @@ const SpotifyConnectedSettings: FC<SpotifyConnectedSettingsProps> = ({ user }) =
   const refreshUserPlaylistsHandler = async () => {
     await fetch('/api/playlists/refresh');
   };
+
+  const reauthorizeSpotifyHandler = () => {
+    const queryString = new URL('https://accounts.spotify.com/authorize');
+    queryString.searchParams.append('response_type', 'code');
+    queryString.searchParams.append('client_id', process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID || '');
+    queryString.searchParams.append('scope', spotifyScopes.join(' '));
+    queryString.searchParams.append(
+      'redirect_uri',
+      process.env.NEXT_PUBLIC_BASE_URL + process.env.SPOTIFY_REDIRECT_ENDPOINT
+    );
+    queryString.searchParams.append('state', 'some-random-state');
+    window.open(queryString.toString(), '_self');
+  };
+
   return (
     <div className="flex flex-col items-center justify-center py-8 gap-4">
       <Image
@@ -41,6 +56,11 @@ const SpotifyConnectedSettings: FC<SpotifyConnectedSettingsProps> = ({ user }) =
           onClick={refreshUserPlaylistsHandler}>
           Refresh user playlists
         </AsyncButton>
+        <button
+          className="p-4 bg-primary rounded-sm cursor-pointer hover:bg-background-interactive"
+          onClick={reauthorizeSpotifyHandler}>
+          Re-authorize Spotify
+        </button>
       </div>
       <a href={user.external_urls.spotify} className="text-primary underline m-3 p-2">
         View Spotify profile
